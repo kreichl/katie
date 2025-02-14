a472413440d676202c00059bf5491f956a29978c
Katie Reichl <reichl.katie@gmail.com>
2025-02-14 16:29:53 -0600

    update to delete duplicates

--- Lead_Gen/delete_rows_with_no_emails.py
+++ Lead_Gen/delete_rows_with_no_emails.py
@@ -7,20 +7,24 @@ output_path = r"C:\Users\reich\Documents\GIT\katie\Lead_Gen\Cleaned_Leads_2025_0
 # Load the CSV file into a DataFrame
 df = pd.read_csv(file_path)
 
-
 # Count the number of rows before cleaning
 initial_row_count = len(df)
 
 # Remove rows where 'Agent Email' column is empty
 df_cleaned = df[df['Agent Email'].notna() & (df['Agent Email'] != '')]
 
+# Remove duplicate emails, keeping only the first occurrence
+df_cleaned = df_cleaned.drop_duplicates(subset=['Agent Email'], keep='first')
+
 # Count the number of rows deleted
 deleted_row_count = initial_row_count - len(df_cleaned)
 
 # Display the number of deleted rows
-print(f"Number of rows deleted: {deleted_row_count}")
+print(f"Rows Deleted:: {deleted_row_count}")
 
-# Save the cleaned DataFrame to a new CSV file with the 'cleaned_' prefix
+# Save the cleaned DataFrame to a new CSV file
 df_cleaned.to_csv(output_path, index=False)
+
+print(f"Total Rows: {len(df_cleaned)}")
 
 print(f"Cleaned file saved as {output_path}")
