900670dd8c0dd49d8c5c9049a4f24157f4c406f8
Katie Reichl <reichl.katie@gmail.com>
2025-07-22 16:52:52 -0500

    updates

--- upwork_scrape/main.py
+++ upwork_scrape/main.py
@@ -1,6 +1,6 @@
 import scrape_and_update_airtable
 
-url = "https://www.upwork.com/jobs/~021917201617214503981"
+url = "https://www.upwork.com/jobs/~021938341131489164962"
 record_id = "recMe1bivSHo0SjxB"
 
 scrape_and_update_airtable.scrape_and_update(url, record_id)
